4326cda32b9ee81e0f6a37adc7006efd65ff7536
Kyungguk Min <kyungguk@me.com>
2021-08-12 10:48:26 +0900

    common: Geometry copied from pic_1d

---
+++ src/common/Geometry.cc
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019, Kyungguk Min
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "Geometry.h"
+
+#include <cmath>
+
+P1D::Geometry::Geometry(Input const &params) noexcept
+{
+    Real const theta = params.theta * M_PI / 180;
+    this->e1         = Vector{ std::cos(theta), std::sin(theta), 0 };
+    this->e2         = Vector{ -std::sin(theta), std::cos(theta), 0 };
+    this->B0         = this->e1 * params.O0;
+}
