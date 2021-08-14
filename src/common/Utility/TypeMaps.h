f5f8512b5a21ed0f851c60c8ec589199eea6649d
Kyungguk Min <kyungguk@me.com>
2021-08-14 13:16:25 +0900

    common+tests: Add TypeMaps

--- src/common/Utility/TypeMaps.h
+++ src/common/Utility/TypeMaps.h
@@ -4,13 +4,12 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#ifndef PIC_1D_TYPEMAPS_H
-#define PIC_1D_TYPEMAPS_H
+#pragma once
 
-#include "./Particle.h"
-#include "./Scalar.h"
-#include "./Tensor.h"
-#include "./Vector.h"
+#include <Utility/Scalar.h>
+#include <Utility/Tensor.h>
+#include <Utility/Vector.h>
+#include <VDF/Particle.h>
 
 #include <HDF5Kit/HDF5Kit.h>
 #include <ParallelKit/ParallelKit.h>
@@ -21,37 +20,59 @@
 // mpi TypeMap Interfaces
 //
 namespace parallel {
-template <> struct TypeMap<P1D::Scalar> {
-    using type = P1D::Scalar;
-    using root = P1D::Real;
+template <> struct TypeMap<common::Scalar> {
+    using type = common::Scalar;
+    using root = common::Real;
     static_assert(sizeof(type) == sizeof(root) && alignof(type) == alignof(root),
                   "Custom TypeMap: invalid type signature");
     [[nodiscard]] auto operator()() const { return make_type<root>(); }
 };
-template <> struct TypeMap<P1D::Vector> {
-    using type = P1D::Vector;
-    using root = std::array<P1D::Real, 3>;
+template <> struct TypeMap<common::Vector> {
+    using type = common::Vector;
+    using root = std::array<common::Real, 4>;
+    static_assert(sizeof(type) == sizeof(root) && alignof(type) == 32,
+                  "Custom TypeMap: invalid type signature");
+    [[nodiscard]] auto operator()() const { return make_type<root>().realigned(alignof(type)); }
+};
+template <> struct TypeMap<common::Tensor> {
+    using type = common::Tensor;
+    using root = std::array<common::Real, 8>;
+    static_assert(sizeof(type) == sizeof(root) && alignof(type) == 32,
+                  "Custom TypeMap: invalid type signature");
+    [[nodiscard]] auto operator()() const { return make_type<root>().realigned(alignof(type)); }
+};
+template <> struct TypeMap<common::Particle::PSD> {
+    using type = common::Particle::PSD;
+    using root = std::array<common::Real, 2>;
     static_assert(sizeof(type) == sizeof(root) && alignof(type) == alignof(root),
                   "Custom TypeMap: invalid type signature");
     [[nodiscard]] auto operator()() const { return make_type<root>(); }
 };
-template <> struct TypeMap<P1D::Tensor> {
-    using type = P1D::Tensor;
-    using root = std::array<P1D::Real, 6>;
-    static_assert(sizeof(type) == sizeof(root) && alignof(type) == alignof(root),
+template <> struct TypeMap<common::Particle> {
+    using type         = common::Particle;
+    using equivalent_t = std::array<common::Real, 8>;
+    static_assert(sizeof(type) == sizeof(equivalent_t) && alignof(type) == 32,
                   "Custom TypeMap: invalid type signature");
-    [[nodiscard]] auto operator()() const { return make_type<root>(); }
-};
-template <> struct TypeMap<P1D::Particle> {
-    using type = P1D::Particle;
     static constexpr type v{};
-
-    [[nodiscard]] auto operator()() const
+    [[nodiscard]] auto    operator()() const
     {
-        auto t = make_type(v.vel, v.pos_x, v.f, v.w);
+        auto t = make_type(v.vel, v.pos_x, v.psd, v.id).realigned(alignof(type));
         if (t.extent().second != sizeof(type))
             throw std::domain_error{ __PRETTY_FUNCTION__ };
-
+        return t;
+    }
+};
+template <> struct TypeMap<common::RelativisticParticle> {
+    using type         = common::RelativisticParticle;
+    using equivalent_t = std::array<common::Real, 8>;
+    static_assert(sizeof(type) == sizeof(equivalent_t) && alignof(type) == 32,
+                  "Custom TypeMap: invalid type signature");
+    static constexpr type v{};
+    [[nodiscard]] auto    operator()() const
+    {
+        auto t = make_type(v.g_vel, v.pos_x, v.psd, v.gamma).realigned(alignof(type));
+        if (t.extent().second != sizeof(type))
+            throw std::domain_error{ __PRETTY_FUNCTION__ };
         return t;
     }
 };
@@ -60,47 +81,66 @@ template <> struct TypeMap<P1D::Particle> {
 // hdf5 TypeMap Interfaces
 //
 namespace hdf5 {
-template <> struct TypeMap<P1D::Scalar> {
-    using type = P1D::Scalar;
-    using root = P1D::Real;
+template <> struct TypeMap<common::Scalar> {
+    using type = common::Scalar;
+    using root = common::Real;
     static_assert(sizeof(type) == sizeof(root) && alignof(type) == alignof(root),
                   "Custom TypeMap: invalid type signature");
     [[nodiscard]] auto operator()() const { return make_type<root>(); }
 };
-template <> struct TypeMap<P1D::Vector> {
-    using type = P1D::Vector;
-    using root = std::array<P1D::Real, 3>;
+template <> struct TypeMap<common::Vector> {
+    using type = common::Vector;
+    using root = std::array<common::Real, 4>;
+    static_assert(sizeof(type) == sizeof(root) && alignof(type) == 32,
+                  "Custom TypeMap: invalid type signature");
+    [[nodiscard]] auto operator()() const { return make_type<root>(); }
+};
+template <> struct TypeMap<common::Tensor> {
+    using type = common::Tensor;
+    using root = std::array<common::Real, 8>;
+    static_assert(sizeof(type) == sizeof(root) && alignof(type) == 32,
+                  "Custom TypeMap: invalid type signature");
+    [[nodiscard]] auto operator()() const { return make_type<root>(); }
+};
+#if 1
+template <> struct TypeMap<common::Particle::PSD> {
+    using type = common::Particle::PSD;
+    using root = std::array<common::Real, 2>;
     static_assert(sizeof(type) == sizeof(root) && alignof(type) == alignof(root),
                   "Custom TypeMap: invalid type signature");
     [[nodiscard]] auto operator()() const { return make_type<root>(); }
 };
-template <> struct TypeMap<P1D::Tensor> {
-    using type = P1D::Tensor;
-    using root = std::array<P1D::Real, 6>;
-    static_assert(sizeof(type) == sizeof(root) && alignof(type) == alignof(root),
+template <> struct TypeMap<common::Particle> {
+    using type         = common::Particle;
+    using equivalent_t = std::array<common::Real, 8>;
+    static_assert(sizeof(type) == sizeof(equivalent_t) && alignof(type) == 32,
                   "Custom TypeMap: invalid type signature");
-    [[nodiscard]] auto operator()() const { return make_type<root>(); }
-};
-#if 0
-template <> struct TypeMap<P1D::Particle> {
-    using type         = P1D::Particle;
-    using equivalent_t = std::array<P1D::Real, 6>;
-    static_assert(sizeof(type) == sizeof(equivalent_t) && alignof(type) == alignof(equivalent_t));
-
-    [[nodiscard]] auto operator()() const
+    static constexpr type v{};
+    [[nodiscard]] auto    operator()() const
     {
         auto t = Type::compound(sizeof(type));
-        {
-            static constexpr type v{};
-            t.insert("vel", HOFFSET(type, vel), make_type(v.vel));
-            t.insert("pos_x", HOFFSET(type, pos_x), make_type(v.pos_x));
-            t.insert("f", HOFFSET(type, f), make_type(v.f));
-            t.insert("w", HOFFSET(type, w), make_type(v.w));
-        }
+        t.insert("vel", HOFFSET(type, vel), make_type(v.vel));
+        t.insert("pos_x", HOFFSET(type, pos_x), make_type(v.pos_x));
+        t.insert("psd", HOFFSET(type, psd), make_type(v.psd));
+        t.insert("id", HOFFSET(type, id), make_type(v.id));
+        return t;
+    }
+};
+template <> struct TypeMap<common::RelativisticParticle> {
+    using type         = common::RelativisticParticle;
+    using equivalent_t = std::array<common::Real, 8>;
+    static_assert(sizeof(type) == sizeof(equivalent_t) && alignof(type) == 32,
+                  "Custom TypeMap: invalid type signature");
+    static constexpr type v{};
+    [[nodiscard]] auto    operator()() const
+    {
+        auto t = Type::compound(sizeof(type));
+        t.insert("g_vel", HOFFSET(type, g_vel), make_type(v.g_vel));
+        t.insert("pos_x", HOFFSET(type, pos_x), make_type(v.pos_x));
+        t.insert("psd", HOFFSET(type, psd), make_type(v.psd));
+        t.insert("gamma", HOFFSET(type, gamma), make_type(v.gamma));
         return t;
     }
 };
 #endif
 } // namespace hdf5
-
-#endif // PIC_1D_TYPEMAPS_H
--- tests/main.cc
+++ tests/main.cc
@@ -4,5 +4,22 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_RUNNER
 #include "catch2/catch.hpp"
+
+#include <ParallelKit/ParallelKit.h>
+
+int main(int argc, char *argv[])
+{
+    using parallel::mpi::Comm;
+
+    // global setup...
+    Comm::init(&argc, &argv);
+
+    int result = Catch::Session().run(argc, argv);
+
+    // global clean-up...
+    Comm::deinit();
+
+    return result;
+}
