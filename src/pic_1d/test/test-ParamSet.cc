49112d8fc30d7765aedc297c21a562ea80e1bf61
Kyungguk Min <kyungguk@me.com>
2021-08-14 16:01:31 +0900

    pic_1d: Add test case for ParamSet

---
+++ src/pic_1d/test/test-ParamSet.cc
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021, Kyungguk Min
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "catch2/catch.hpp"
+
+#include <ParamSet.h>
+#include <exception>
+
+using namespace P1D;
+
+TEST_CASE("Test pic_1d::ParamSet", "[pic_1d::ParamSet]")
+{
+    try {
+        auto opts = Options{};
+        opts.parse({ "--wd", "~/Downloads ", "-save ", "--load=false", "--outer_Nt=10" });
+
+        auto const params = ParamSet{ 0, opts };
+        CHECK(params.outer_Nt == 10);
+        CHECK(params.working_directory == "~/Downloads");
+        CHECK(params.snapshot_save == true);
+        CHECK(params.snapshot_load == false);
+    } catch (std::exception const &e) {
+        INFO(e.what());
+        CHECK(false);
+    }
+}
