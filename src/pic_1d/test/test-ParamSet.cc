6334e8c33916022443f6c0d72af523e4338308b0
Kyungguk Min <kyungguk@me.com>
2021-08-15 14:23:27 +0900

    test-pic_1d: Add params.domain_extent check

--- src/pic_1d/test/test-ParamSet.cc
+++ src/pic_1d/test/test-ParamSet.cc
@@ -22,6 +22,8 @@ TEST_CASE("Test pic_1d::ParamSet", "[pic_1d::ParamSet]")
         CHECK(params.working_directory == "~/Downloads");
         CHECK(params.snapshot_save == true);
         CHECK(params.snapshot_load == false);
+        CHECK(params.domain_extent.loc == 0);
+        CHECK(params.domain_extent.len == params.Nx / params.number_of_subdomains);
     } catch (std::exception const &e) {
         INFO(e.what());
         CHECK(false);
