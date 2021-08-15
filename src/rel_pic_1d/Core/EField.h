38c07ec958636306c5cea5c63fbab0f124f13227
Kyungguk Min <kyungguk@me.com>
2021-08-15 18:53:06 +0900

    rel_pic_1d: Update BField, EField, Current, and Species

--- src/rel_pic_1d/Core/EField.h
+++ src/rel_pic_1d/Core/EField.h
@@ -4,10 +4,8 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#ifndef EField_h
-#define EField_h
+#pragma once
 
-#include "../Geometry.h"
 #include "../ParamSet.h"
 
 #include <HDF5Kit/HDF5Kit.h>
@@ -19,17 +17,17 @@ class Current;
 class EField : public VectorGrid {
 public:
     ParamSet const params;
-    Geometry const geomtr;
 
-public:
     explicit EField(ParamSet const &);
 
     void update(BField const &bfield, Current const &current, Real dt) noexcept;
 
 private:
-    static inline void _update(EField &E, BField const &B, Real cdtODx, Current const &J,
-                               Real dt) noexcept;
+    static void impl_update(EField &E, BField const &B, Real cdtODx, Current const &J,
+                            Real dt) noexcept;
 
+    // attribute export facility
+    //
     friend auto operator<<(hdf5::Group &obj, EField const &efield) -> decltype(obj);
     friend auto operator<<(hdf5::Dataset &obj, EField const &efield) -> decltype(obj);
     friend auto operator<<(hdf5::Group &&obj, EField const &efield) -> decltype(obj)
@@ -42,5 +40,3 @@ private:
     }
 };
 PIC1D_END_NAMESPACE
-
-#endif /* EField_h */
