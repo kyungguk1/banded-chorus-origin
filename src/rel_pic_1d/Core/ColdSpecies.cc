2c7c488e650c8591b86f2c86b2aa627f4e19c5db
Kyungguk Min <kyungguk@me.com>
2021-08-15 19:36:05 +0900

    rel_pic_1d: Update concrete Species

--- src/rel_pic_1d/Core/ColdSpecies.cc
+++ src/rel_pic_1d/Core/ColdSpecies.cc
@@ -5,17 +5,17 @@
  */
 
 #include "ColdSpecies.h"
-
-#include "./BField.h"
-#include "./EField.h"
+#include "BField.h"
+#include "EField.h"
 
 #include <algorithm>
 
-P1D::ColdSpecies::ColdSpecies(ParamSet const &params, ColdPlasmaDesc const &desc)
+PIC1D_BEGIN_NAMESPACE
+ColdSpecies::ColdSpecies(ParamSet const &params, ColdPlasmaDesc const &desc)
 : Species{ params }, desc{ desc }
 {
 }
-void P1D::ColdSpecies::populate()
+void ColdSpecies::populate()
 {
     // initialize equilibrium moments
     //
@@ -23,36 +23,36 @@ void P1D::ColdSpecies::populate()
     auto &nV = mom1_full;
     //
     constexpr Scalar n0{ 1 };
-    Vector const     nV0 = Real{ n0 } * desc.Vd / params.O0 * geomtr.B0;
+    Vector const     nV0 = Real{ n0 } * desc.Vd / params.O0 * params.geomtr.B0;
     for (long i = 0; i < nV.size(); ++i) { // only the interior
         n[i]  = n0;
         nV[i] = nV0;
     }
 }
 
-void P1D::ColdSpecies::update_vel(BField const &bfield, EField const &efield, Real const dt)
+void ColdSpecies::update_vel(BField const &bfield, EField const &efield, Real const dt)
 {
-    _update_nV(mom1_full, mom0_full, bfield.geomtr.B0, efield,
-               BorisPush{ dt, params.c, params.O0, desc.Oc });
+    impl_update_nV(mom1_full, mom0_full, bfield.params.geomtr.B0, efield,
+                   BorisPush{ dt, params.c, params.O0, desc.Oc });
 }
-void P1D::ColdSpecies::_update_nV(VectorGrid &nV, ScalarGrid const &n, Vector const &B0,
-                                  EField const &E, BorisPush const pusher) const
+void ColdSpecies::impl_update_nV(VectorGrid &nV, ScalarGrid const &n, Vector const &B0,
+                                 EField const &E, BorisPush const &boris) const
 {
     for (long i = 0; i < nV.size(); ++i) {
-        pusher.non_relativistic(nV[i], B0, E[i] * Real{ n[i] });
+        boris.non_relativistic(nV[i], B0, E[i] * Real{ n[i] });
     }
 }
 
-void P1D::ColdSpecies::collect_part()
+void ColdSpecies::collect_part()
 {
-    _collect_part(moment<0>(), moment<1>());
+    impl_collect_part(moment<0>(), moment<1>());
 }
-void P1D::ColdSpecies::collect_all()
+void ColdSpecies::collect_all()
 {
-    _collect_part(moment<0>(), moment<1>());
-    _collect_nvv(moment<2>(), moment<0>(), moment<1>());
+    impl_collect_part(moment<0>(), moment<1>());
+    impl_collect_nvv(moment<2>(), moment<0>(), moment<1>());
 }
-void P1D::ColdSpecies::_collect_part(ScalarGrid &n, VectorGrid &nV) const
+void ColdSpecies::impl_collect_part(ScalarGrid &n, VectorGrid &nV) const
 {
     // must zero-out ghost cells
     //
@@ -62,7 +62,7 @@ void P1D::ColdSpecies::_collect_part(ScalarGrid &n, VectorGrid &nV) const
     nV.fill(Vector{});
     std::copy(mom1_full.begin(), mom1_full.end(), nV.begin());
 }
-void P1D::ColdSpecies::_collect_nvv(TensorGrid &nvv, ScalarGrid const &n, VectorGrid const &nV)
+void ColdSpecies::impl_collect_nvv(TensorGrid &nvv, ScalarGrid const &n, VectorGrid const &nV)
 {
     for (long i = 0; i < nV.size(); ++i) {
         Tensor       &nvvi = nvv[i];
@@ -74,3 +74,4 @@ void P1D::ColdSpecies::_collect_nvv(TensorGrid &nvv, ScalarGrid const &n, Vector
         nvvi.hi() *= { nVi.y, nVi.z, nVi.x }; // off-diag terms
     }
 }
+PIC1D_END_NAMESPACE
--- src/rel_pic_1d/Core/ColdSpecies.h
+++ src/rel_pic_1d/Core/ColdSpecies.h
@@ -1,13 +1,12 @@
 /*
- * Copyright (c) 2019, Kyungguk Min
+ * Copyright (c) 2019-2021, Kyungguk Min
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#ifndef ColdSpecies_h
-#define ColdSpecies_h
+#pragma once
 
-#include "./Species.h"
+#include "Species.h"
 
 PIC1D_BEGIN_NAMESPACE
 class EField;
@@ -19,15 +18,14 @@ class ColdSpecies : public Species {
     ColdPlasmaDesc desc;
 
 public:
+    [[nodiscard]] ColdPlasmaDesc const *operator->() const noexcept override { return &desc; }
+
     ScalarGrid mom0_full{}; // 0th moment on full grid
     VectorGrid mom1_full{}; // 1st moment on full grid
 
-public:
-    [[nodiscard]] ColdPlasmaDesc const *operator->() const noexcept override { return &desc; }
-
-    ColdSpecies &operator=(ColdSpecies &&) = delete;
-    ColdSpecies()                          = default; // needed for empty std::array
+    ColdSpecies &operator=(ColdSpecies &&) = delete; // this should not be default-ed
     ColdSpecies(ParamSet const &params, ColdPlasmaDesc const &desc);
+    ColdSpecies() = default; // needed for empty std::array
 
     void populate(); // load cold species; should only be called by master thread
 
@@ -38,12 +36,10 @@ public:
     void collect_all();  // collect all moments
 
 private:
-    void _update_nV(VectorGrid &nV, ScalarGrid const &n, Vector const &B0, EField const &E,
-                    BorisPush pusher) const;
+    void impl_update_nV(VectorGrid &nV, ScalarGrid const &n, Vector const &B0, EField const &E,
+                        BorisPush const &boris) const;
 
-    void        _collect_part(ScalarGrid &n, VectorGrid &nV) const;
-    static void _collect_nvv(TensorGrid &nvv, ScalarGrid const &n, VectorGrid const &nV);
+    void        impl_collect_part(ScalarGrid &n, VectorGrid &nV) const;
+    static void impl_collect_nvv(TensorGrid &nvv, ScalarGrid const &n, VectorGrid const &nV);
 };
 PIC1D_END_NAMESPACE
-
-#endif /* ColdSpecies_h */
