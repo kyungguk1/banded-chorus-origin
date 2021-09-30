2b3675fefa51fc1461b5c0d68725ad6cfd842b93
Kyungguk Min <kyungguk@me.com>
2021-10-01 08:50:13 +0900

    libPIC: Add a test case for MaxwellianVDF with homogeneous field

--- src/libPIC/test/test-VDF.cc
+++ src/libPIC/test/test-VDF.cc
@@ -14,10 +14,68 @@
 #include <fstream>
 
 namespace {
-constexpr bool dump_samples = true;
+constexpr bool dump_samples = false;
 }
 
-// TODO: Test homogeneous case.
+TEST_CASE("Test libPIC::MaxwellianVDF::Homogeneous", "[libPIC::MaxwellianVDF::Homogeneous]")
+{
+    Real const O0 = 1., op = 4 * O0, c = op, beta1_eq = .1, T2OT1_eq = 5.35;
+    Real const xi = 0, D1 = 1;
+    long const q1min = -7, q1max = 15;
+    auto const geo  = Geometry{ xi, D1, O0 };
+    auto const desc = BiMaxPlasmaDesc({ { -O0, op }, 10, ShapeOrder::CIC }, beta1_eq, T2OT1_eq);
+    auto const vdf  = MaxwellianVDF(desc, geo, { q1min, q1max - q1min }, c);
+
+    CHECK(serialize(desc) == serialize(vdf.plasma_desc()));
+
+    // check equilibrium macro variables
+    CHECK(vdf.Nrefcell_div_Ntotal() == Approx{ 1.0 / (q1max - q1min) }.epsilon(1e-10));
+
+    for (long q1 = q1min; q1 <= q1max; ++q1) {
+        CurviCoord const pos{ Real(q1) };
+        auto const       eta = 1;
+
+        auto const n0_ref = eta;
+        auto const n0     = vdf.n0(pos);
+        CHECK(*n0 == Approx{ n0_ref }.epsilon(1e-10));
+
+        auto const nV0 = geo.cart_to_fac(vdf.nV0(pos), pos);
+        CHECK(nV0.x == Approx{ 0 }.margin(1e-10));
+        CHECK(nV0.y == Approx{ 0 }.margin(1e-10));
+        CHECK(nV0.z == Approx{ 0 }.margin(1e-10));
+
+        auto const nvv0 = geo.cart_to_fac(vdf.nvv0(pos), pos);
+        CHECK(nvv0.xx == Approx{ desc.beta1 / 2 * eta }.epsilon(1e-10));
+        CHECK(nvv0.yy == Approx{ desc.beta1 / 2 * desc.T2_T1 * eta * eta }.epsilon(1e-10));
+        CHECK(nvv0.zz == Approx{ desc.beta1 / 2 * desc.T2_T1 * eta * eta }.epsilon(1e-10));
+        CHECK(nvv0.xy == Approx{ 0 }.margin(1e-10));
+        CHECK(nvv0.yz == Approx{ 0 }.margin(1e-10));
+        CHECK(nvv0.zx == Approx{ 0 }.margin(1e-10));
+    }
+
+    // sampling
+    auto const n_samples = 100000U;
+    auto const particles = vdf.emit(n_samples);
+
+    static_assert(n_samples > 10);
+    std::for_each_n(begin(particles), 10, [](auto const &ptl) {
+        REQUIRE(ptl.psd.weight == 1);
+        REQUIRE(ptl.psd.real_f == -1);
+        REQUIRE(ptl.psd.marker == -1);
+    });
+
+    if constexpr (dump_samples) {
+        std::ofstream os{ "/Users/kyungguk/Downloads/MaxwellianVDF-homogeneous.m" };
+        static_assert(n_samples > 0);
+        println(os, '{');
+        for (unsigned long i = 0; i < particles.size() - 1; ++i) {
+            println(os, "    ", particles[i], ", ");
+        }
+        println(os, "    ", particles.back());
+        println(os, '}');
+        os.close();
+    }
+}
 
 TEST_CASE("Test libPIC::MaxwellianVDF::full_f", "[libPIC::MaxwellianVDF::full_f]")
 {
