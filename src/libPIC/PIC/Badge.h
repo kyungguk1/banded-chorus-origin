5681a094f8df4e9f1df87799b1e3d9e9d975151f
Kyungguk Min <kyungguk@me.com>
2021-10-05 08:04:03 +0900

    libPIC: Add Badge
    Limiting member function access to those holding a Badge is taken from
    AK framework of SerenityOS.

---
+++ src/libPIC/PIC/Badge.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, Kyungguk Min
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <PIC/Config.h>
+
+LIBPIC_BEGIN_NAMESPACE
+template <class Holder>
+class Badge {
+    friend Holder;
+
+    constexpr Badge() noexcept = default;
+
+public:
+    Badge(Badge const &) noexcept = delete;
+    Badge &operator=(Badge const &) noexcept = delete;
+};
+LIBPIC_END_NAMESPACE
