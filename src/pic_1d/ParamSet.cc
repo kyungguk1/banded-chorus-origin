0cdb1a8fc1bc2d2b6b7bfec1e14388674421eb83
Kyungguk Min <kyungguk@me.com>
2021-10-05 18:54:29 +0900

    pic_1d/ParamSet: Save full_grid_domain_extent as 2-element 1D dataset
    Previously, the value of this attribute is saved as a scalar H5T_ARRAY
    type.

--- src/pic_1d/ParamSet.cc
+++ src/pic_1d/ParamSet.cc
@@ -6,6 +6,7 @@
 
 #include "ParamSet.h"
 
+#include <array>
 #include <map>
 #include <stdexcept>
 #include <variant>
@@ -57,8 +58,10 @@ decltype(auto) write_attr(Object &obj, ParamSet const &params)
     using hdf5::make_type;
     using hdf5::Space;
     { // global parameters
-        obj.attribute("full_grid_domain_extent", make_type(params.full_grid_whole_domain_extent.minmax()), Space::scalar())
-            .write(params.full_grid_whole_domain_extent.minmax());
+        std::array const extent{ params.full_grid_whole_domain_extent.min(), params.full_grid_whole_domain_extent.max() };
+        auto const       type = make_type(extent.front());
+        obj.attribute("full_grid_domain_extent", type, Space::simple(extent.size()))
+            .write(extent.data(), type);
         obj.attribute("number_of_worker_threads", make_type(params.number_of_worker_threads), Space::scalar())
             .write(params.number_of_worker_threads);
         obj.attribute("number_of_subdomains", make_type(params.number_of_subdomains), Space::scalar())
