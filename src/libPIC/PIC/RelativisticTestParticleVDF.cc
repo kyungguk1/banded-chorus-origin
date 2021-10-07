132bb057e0c4c0c99edf4369976260692444af1b
Kyungguk Min <kyungguk@me.com>
2021-10-07 13:42:58 +0900

    libPIC: Implement RelativisticTestParticleVDF

---
+++ src/libPIC/PIC/RelativisticTestParticleVDF.cc
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2021, Kyungguk Min
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "RelativisticTestParticleVDF.h"
+
+LIBPIC_BEGIN_NAMESPACE
+auto RelativisticTestParticleVDF::impl_emit(unsigned long const n) const -> std::vector<Particle>
+{
+    std::vector<Particle> ptls(n);
+    for (auto &ptl : ptls)
+        ptl = emit();
+    return ptls;
+}
+auto RelativisticTestParticleVDF::impl_emit() const -> Particle
+{
+    Particle ptl = load();
+    {
+        ptl.psd = { 1, -1, -1 };
+    }
+    return ptl;
+}
+auto RelativisticTestParticleVDF::load() const -> Particle
+{
+    if (particles.empty())
+        return {}; // this assumes that the default-constructed particle object is not consumed by callers
+
+    auto ptl = particles.back();
+    particles.pop_back();
+    return ptl;
+}
+LIBPIC_END_NAMESPACE
---
+++ src/libPIC/PIC/RelativisticTestParticleVDF.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2021, Kyungguk Min
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <PIC/RelativisticVDF.h>
+#include <algorithm>
+#include <cmath>
+#include <iterator>
+
+LIBPIC_BEGIN_NAMESPACE
+/// Test particle VDF
+/// \details The sole job of this object is to dispense particles initialized with
+///          the velocity and position passed by a TestParticleDesc object.
+///
+///          When all particles are exhausted, any further inquiry to the emit functions
+///          returns a default-constructed Particle object which callers should ignore.
+class RelativisticTestParticleVDF : public RelativisticVDF<RelativisticTestParticleVDF> {
+    friend RelativisticVDF<RelativisticTestParticleVDF>;
+
+    static constexpr Real         m_Nrefcell_div_Ntotal{ 1 };
+    KineticPlasmaDesc             desc;
+    mutable std::vector<Particle> particles; // holder for remaining particles
+    unsigned                      initial_number_of_test_particles;
+
+public:
+    /// Construct a test particle generator
+    /// \tparam N The number of test particles.
+    /// \param desc A TestParticleDesc object.
+    /// \param geo A geometry object.
+    /// \param domain_extent Spatial domain extent.
+    /// \param c Light speed. A positive real.
+    template <unsigned N>
+    RelativisticTestParticleVDF(TestParticleDesc<N> const &desc, Geometry const &geo, Range const &domain_extent, Real c)
+    : RelativisticVDF{ geo, domain_extent, c }, desc{ desc }, particles(N), initial_number_of_test_particles{ N }
+    {
+        std::transform(
+            begin(desc.vel), end(desc.vel), begin(desc.pos), rbegin(particles) /*reverse order*/,
+            [c2 = this->c2](Vector const &g_vel, CurviCoord const &pos) -> Particle {
+                return { g_vel, pos, std::sqrt(1 + dot(g_vel, g_vel) / c2) };
+            });
+    }
+
+private:
+    [[nodiscard]] decltype(auto) impl_plasma_desc() const noexcept { return (this->desc); }
+
+    [[nodiscard]] Scalar     impl_n(CurviCoord const &) const { return 0; }
+    [[nodiscard]] Vector     impl_nV(CurviCoord const &) const { return { 0, 0, 0 }; }
+    [[nodiscard]] FourTensor impl_nuv(CurviCoord const &) const { return { 0, { 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 } }; }
+
+    [[nodiscard]] Real impl_weight(Particle const &) const { return 0; }
+
+    [[nodiscard]] std::vector<Particle> impl_emit(unsigned long) const;
+    [[nodiscard]] Particle              impl_emit() const;
+    [[nodiscard]] Particle              load() const;
+
+public:
+    // equilibrium physical distribution function
+    //
+    [[nodiscard]] Real f0(Vector const &, CurviCoord const &) const noexcept { return 0; }
+    [[nodiscard]] Real f0(Particle const &ptl) const noexcept { return f0(ptl.g_vel, ptl.pos); }
+
+    // marker particle distribution function
+    //
+    [[nodiscard]] Real g0(Vector const &, CurviCoord const &) const noexcept { return 0; }
+    [[nodiscard]] Real g0(Particle const &ptl) const noexcept { return g0(ptl.g_vel, ptl.pos); }
+};
+LIBPIC_END_NAMESPACE
