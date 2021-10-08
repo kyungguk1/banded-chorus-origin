0c2cbeedcd4e8fce3a02582a9bece46af7d6fa12
Kyungguk Min <kyungguk@me.com>
2021-10-09 08:19:07 +0900

    pic_1d: Add a skeletal implementation of Driver which uses the distributed particle scheme

---
+++ src/pic_1d/DistributedDriver.cc
@@ -0,0 +1,176 @@
+/*
+ * Copyright (c) 2021, Kyungguk Min
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include "DistributedDriver.h"
+#include "Boundary/DistributedParticleDelegate.h"
+#include "Boundary/SubdomainDelegate.h"
+#include "Recorder/EnergyRecorder.h"
+#include "Recorder/FieldRecorder.h"
+#include "Recorder/MomentRecorder.h"
+#include "Recorder/ParticleRecorder.h"
+#include "Recorder/Snapshot.h"
+#include "Recorder/VHistogramRecorder.h"
+#include <PIC/lippincott.h>
+#include <PIC/println.h>
+
+#include <chrono>
+#include <exception>
+#include <iostream>
+
+PIC1D_BEGIN_NAMESPACE
+namespace Distributed {
+namespace {
+    template <class F, class... Args>
+    [[nodiscard]] auto measure(F &&f, Args &&...args) -> std::chrono::duration<double>
+    {
+        static_assert(std::is_invocable_v<F &&, Args &&...>);
+        auto const start = std::chrono::steady_clock::now();
+        {
+            std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
+        }
+        auto const end = std::chrono::steady_clock::now();
+
+        return end - start;
+    }
+} // namespace
+
+Driver::~Driver()
+{
+}
+Driver::Driver(ParamSet const &params, parallel::mpi::Comm _comm)
+: params{ params }, world{ std::move(_comm) }
+{
+    try {
+        if (!world)
+            fatal_error(__PRETTY_FUNCTION__, " - invalid mpi::Comm object");
+
+        if (auto const size = world.size(); size != params.number_of_subdomains)
+            fatal_error(__PRETTY_FUNCTION__, " - the mpi world size (= ", std::to_string(size),
+                        ") is not the same as number_of_subdomains (= ", std::to_string(params.number_of_subdomains), ')');
+
+        auto const world_rank = world.rank();
+
+        // group comm's
+        // say, there are 3 subdomains
+        // then the grouping of subdomain_comm's are {0, 1, 2}, {3, 4, 5}, ...
+        // and the grouping of distributed_particle_comm's are {0, 3, 6, ...}, {1, 4, 7, ...}, and {2, 5, 8, ...}
+        //
+        subdomain_comm = world.split(world_rank / long{ params.number_of_subdomains });
+        if (subdomain_comm.size() != params.number_of_subdomains) // TODO: Remove this once verified
+            fatal_error(__PRETTY_FUNCTION__, " - invalid subdomain_comm size");
+        distributed_particle_comm = world.split(world_rank % long{ params.number_of_subdomains });
+        if (distributed_particle_comm.size() != params.number_of_particle_parallelism) // TODO: Remove this once verified
+            fatal_error(__PRETTY_FUNCTION__, " - invalid distributed_particle_comm size");
+
+        // TODO: init recorders
+        //
+        // recorders["energy"]    = std::make_unique<EnergyRecorder>(comm.duplicated(), params);
+        // recorders["fields"]    = std::make_unique<FieldRecorder>(comm.duplicated());
+        // recorders["moment"]    = std::make_unique<MomentRecorder>(comm.duplicated());
+        // recorders["vhists"]    = std::make_unique<VHistogramRecorder>(comm.duplicated());
+        // recorders["particles"] = std::make_unique<ParticleRecorder>(comm.duplicated());
+
+        // init delegates
+        //
+        subdomain_delegate            = std::make_unique<SubdomainDelegate>(subdomain_comm.duplicated());
+        distributed_particle_delegate = std::make_unique<DistributedParticleDelegate>(distributed_particle_comm.duplicated(), subdomain_delegate.get());
+
+        // init domain
+        //
+        if (0 == world_rank)
+            println(std::cout, __FUNCTION__, "> initializing domain(s)");
+        domain = make_domain(params, distributed_particle_delegate.get());
+
+        // init particles or load snapshot
+        //
+        if (params.snapshot_load) {
+            if (0 == world_rank)
+                print(std::cout, "\tloading snapshots") << std::endl;
+            // TODO: Construct Snapshot
+            // iteration_count = load(Snapshot{ comm.duplicated(), params }, *domain);
+        } else {
+            if (0 == world_rank)
+                print(std::cout, "\tinitializing particles") << std::endl;
+
+            // TODO: Communicate to Species how to divide up
+            for (PartSpecies &sp : domain->part_species) {
+                sp.populate();
+            }
+            for (ColdSpecies &sp : domain->cold_species) {
+                sp.populate();
+            }
+
+            /* TODO: Implement this
+            if (params.record_particle_at_init) {
+                // first, collect particle moments
+                for (PartSpecies &sp : domain->part_species) {
+                    sp.collect_all();
+                    delegate->gather(*domain, sp);
+                }
+
+                // then, dump
+                if (auto const &recorder = recorders.at("particles"))
+                    recorder->record(*domain, iteration_count);
+                if (auto const &recorder = recorders.at("vhists"))
+                    recorder->record(*domain, iteration_count);
+            }
+             */
+        }
+    } catch (std::exception const &e) {
+        fatal_error(__PRETTY_FUNCTION__, " :: ", e.what());
+    }
+}
+auto Driver::make_domain(ParamSet const &params, Delegate *delegate) -> std::unique_ptr<Domain>
+{
+    return std::make_unique<Domain>(params, delegate);
+}
+
+void Driver::operator()()
+try {
+    auto const elapsed = measure(&Driver::update, this);
+    if (0 == world.rank())
+        println(std::cout, "%% time elapsed: ", elapsed.count(), 's');
+
+    // TODO: take snapshot
+    /*
+    if (params.snapshot_save) {
+        if (0 == world.rank())
+            print(std::cout, "\tsaving snapshots") << std::endl;
+        save(Snapshot{ world.duplicated(), params }, *domain, iteration_count);
+    }
+     */
+} catch (std::exception const &e) {
+    fatal_error(__PRETTY_FUNCTION__, " :: ", e.what());
+}
+void Driver::update()
+try {
+    for (long outer_step = 1; outer_step <= params.outer_Nt; ++outer_step) {
+        if (0 == world.rank())
+            println(std::cout, __FUNCTION__, "> ",
+                    "steps(x", params.inner_Nt, ") = ", outer_step, "/", params.outer_Nt,
+                    "; time = ", iteration_count * params.dt);
+
+        // inner loop
+        //
+        domain->advance_by(params.inner_Nt);
+
+        // increment step count
+        //
+        iteration_count += params.inner_Nt;
+
+        // TODO: record data
+        /*
+        for (auto &pair : recorders) {
+            if (pair.second)
+                pair.second->record(*domain, iteration_count);
+        }
+         */
+    }
+} catch (std::exception const &e) {
+    fatal_error(__PRETTY_FUNCTION__, " :: ", e.what());
+}
+} // namespace Distributed
+PIC1D_END_NAMESPACE
---
+++ src/pic_1d/DistributedDriver.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2021, Kyungguk Min
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include "Boundary/Delegate.h"
+#include "Core/Domain.h"
+#include "ParamSet.h"
+#include "Recorder/Recorder.h"
+
+#include <ParallelKit/ParallelKit.h>
+#include <map>
+#include <memory>
+#include <string>
+
+PIC1D_BEGIN_NAMESPACE
+namespace Distributed {
+class [[nodiscard]] Driver {
+    long                      iteration_count{};
+    ParamSet const            params;
+    parallel::mpi::Comm const world;
+    parallel::mpi::Comm       subdomain_comm;
+    parallel::mpi::Comm       distributed_particle_comm;
+    std::unique_ptr<Domain>   domain;
+    std::unique_ptr<Delegate> subdomain_delegate;
+    std::unique_ptr<Delegate> distributed_particle_delegate;
+    // std::map<std::string, std::unique_ptr<Recorder>> recorders;
+
+public:
+    ~Driver();
+    Driver(ParamSet const &params, parallel::mpi::Comm comm);
+    Driver(Driver const &) = delete;
+
+    void operator()();
+
+private:
+    void update();
+
+    [[nodiscard]] static std::unique_ptr<Domain> make_domain(ParamSet const &params, Delegate *delegate);
+};
+} // namespace Distributed
+PIC1D_END_NAMESPACE
