176a749cef5a0906739a6260f235f0a961c7981e
Kyungguk Min <kyungguk@me.com>
2021-10-09 22:26:31 +0900

    pic_1d+rel_pic_1d: Removal of extraneous comment in FieldRecorder::convert

--- src/rel_pic_1d/Recorder/FieldRecorder.h
+++ src/rel_pic_1d/Recorder/FieldRecorder.h
@@ -32,7 +32,6 @@ private:
     template <class T>
     static auto write_data(std::vector<T> payload, hdf5::Group &root, char const *name);
 
-    // this simply wraps Grid quantity into std::vector
     [[nodiscard]] static std::vector<Vector> convert(VectorGrid const &field, Geometry const &) { return { field.begin(), field.end() }; }
 };
 PIC1D_END_NAMESPACE
