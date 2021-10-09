5a4c461686691976c65c39aee2670abac145cf95
Kyungguk Min <kyungguk@me.com>
2021-10-09 18:27:38 +0900

    hybrid_1d: Update Domain_PC and Domain_CAMCL

--- src/hybrid_1d/Core/Domain_CAMCL.h
+++ src/hybrid_1d/Core/Domain_CAMCL.h
@@ -19,7 +19,7 @@ class Domain_CAMCL : public Domain {
     Gamma   gamma;
 
 public:
-    explicit Domain_CAMCL(ParamSet const &params, Delegate *delegate);
+    Domain_CAMCL(ParamSet const &params, Delegate *delegate);
 
 private:
     void advance_by(unsigned n_steps) override;
