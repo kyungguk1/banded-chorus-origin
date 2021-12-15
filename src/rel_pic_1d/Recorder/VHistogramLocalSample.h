3027df9edf0f1bd76fac43df02df3903f44d9a60
Kyungguk Min <kyungguk@me.com>
2021-12-15 09:02:55 +0900

    rel_pic_1d: Add physical particle histogram to the psd list in VHistogramRecorder

---
+++ src/rel_pic_1d/Recorder/VHistogramLocalSample.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021, Kyungguk Min
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include "../Macros.h"
+#include <PIC/Predefined.h>
+#include <ParallelKit/ParallelKit.h>
+
+PIC1D_BEGIN_NAMESPACE
+struct LocalSample {
+    unsigned long marker{}; // marker particle count
+    Real          weight{}; // weight count
+    Real          real_f{}; // full-f count
+};
+PIC1D_END_NAMESPACE
+
+namespace parallel {
+template <>
+struct TypeMap<P1D::LocalSample> {
+    [[nodiscard]] auto operator()() const
+    {
+        constexpr auto value = P1D::LocalSample{};
+        return make_type(value.marker, value.weight, value.real_f);
+    }
+};
+} // namespace parallel
