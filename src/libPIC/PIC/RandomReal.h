029f3f104b9fcd56b6662eac2dabebf539412c6b
Kyungguk Min <kyungguk@me.com>
2022-02-03 10:44:25 +0900

    libPIC: Restructure the encapsulation of the uniform real generation
    Previously, the persistent random engines are put in place in the
    templated function in the header file. This commit puts them into a
    separate translation unit to avoid an accidental repeat of the random
    numbers.

--- src/libPIC/PIC/RandomReal.h
+++ src/libPIC/PIC/RandomReal.h
@@ -13,55 +13,87 @@
 #include <PIC/Predefined.h>
 
 #include <random>
+#include <variant>
 
 LIBPIC_BEGIN_NAMESPACE
-// given a rng object, returns a real number, (0, 1), following a uniform distribution
-//
-template <class URBG>
-[[nodiscard]] Real uniform_real(URBG &g) noexcept
-{
-    using uniform_t                   = std::uniform_real_distribution<>;
-    static constexpr Real         eps = 1e-15;
-    thread_local static uniform_t uniform{ eps, 1 - eps };
-    return uniform(g);
-}
+/// Object representing a persistent random real engine
+///
+class RandomReal final {
+public:
+    using engine_t = std::mt19937;
+    // using engine_t = NRRandomEngine;
+    // using engine_t = xoroshiro128;
 
-template <unsigned seed>
-[[nodiscard]] Real uniform_mt19937() noexcept
-{
-    thread_local static std::mt19937 g{ seed };
-    return uniform_real(g);
-}
-template <unsigned seed>
-[[nodiscard]] Real uniform_xoroshiro128() noexcept
-{
-    thread_local static xoroshiro128 g{ seed };
-    return uniform_real(g);
-}
-template <unsigned seed>
-[[nodiscard]] Real uniform_nr_random() noexcept
-{
-    thread_local static NRRandomEngine g{ seed };
-    return uniform_real(g);
-}
+    [[nodiscard]] Real operator()()
+    {
+        return m_dist(*m_engine);
+    }
+
+    explicit RandomReal(unsigned seed);
+    RandomReal(RandomReal const &) = delete;
+    RandomReal &operator=(RandomReal const &) = delete;
+
+private:
+    engine_t                            *m_engine{};
+    std::uniform_real_distribution<Real> m_dist{ eps, 1 - eps };
+    static constexpr Real                eps = 1e-15;
+};
+
+/// Object representing a persistent bit-reversed bit pattern engine
+///
+class BitReversed final {
+public:
+    using engine_t = std::variant<
+        BitReversedPattern<2U>,
+        BitReversedPattern<3U>,
+        BitReversedPattern<5U>,
+        BitReversedPattern<7U>,
+        BitReversedPattern<11U>,
+        BitReversedPattern<13U>,
+        BitReversedPattern<17U>,
+        BitReversedPattern<19U>,
+        BitReversedPattern<23U>,
+        BitReversedPattern<29U>>;
+
+    [[nodiscard]] Real operator()()
+    {
+        return std::visit(
+            [this](auto &engine) {
+                return m_dist(engine);
+            },
+            *m_engine);
+    }
+
+    explicit BitReversed(unsigned base);
+    BitReversed(BitReversed const &) = delete;
+    BitReversed &operator=(BitReversed const &) = delete;
+
+private:
+    engine_t                            *m_engine{};
+    std::uniform_real_distribution<Real> m_dist{ eps, 1 - eps };
+    static constexpr Real                eps = 1e-15;
+
+    template <class... Types>
+    auto bit_reversed_pool(std::variant<Types...> *);
+};
 
 /// Returns a real number (0, 1) following a uniform distribution
 /// \tparam seed A seed for a random number generator.
 ///
 template <unsigned seed>
-[[nodiscard]] Real uniform_real() noexcept
-{ // seed must be passed as a template parameter
-    return uniform_mt19937<seed>();
+[[nodiscard]] Real uniform_real()
+{
+    thread_local static auto rng = RandomReal{ seed };
+    return rng();
 }
 
 /// Returns a real number (0, 1) following a uniform distribution
 /// \tparam base Base prime number for BitReversedPattern.
 ///
 template <unsigned base>
-[[nodiscard]] Real bit_reversed() noexcept
+[[nodiscard]] Real bit_reversed()
 {
-    static_assert(base > 0, "base has to be a positive number");
-    thread_local static BitReversedPattern<base> g{};
-    return uniform_real(g);
+    thread_local static auto rng = BitReversed{ base };
+    return rng();
 }
 LIBPIC_END_NAMESPACE
