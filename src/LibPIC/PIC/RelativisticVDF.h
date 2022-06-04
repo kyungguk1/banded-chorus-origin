82155d66667493b279a4039ac37cf3d82ed8d701
Kyungguk Min <kyungguk@me.com>
2022-06-05 06:43:05 +0900

    LibPIC: Copy VDF and RelativisticVDF from toy-pic_1d fbbeb965

--- src/LibPIC/PIC/RelativisticVDF.h
+++ src/LibPIC/PIC/RelativisticVDF.h
@@ -8,15 +8,16 @@
 
 #include <PIC/Config.h>
 #include <PIC/CurviCoord.h>
-#include <PIC/FourTensor.h>
 #include <PIC/Geometry.h>
 #include <PIC/PlasmaDesc.h>
 #include <PIC/Predefined.h>
-#include <PIC/Range.h>
 #include <PIC/RelativisticParticle.h>
-#include <PIC/Scalar.h>
-#include <PIC/Tensor.h>
-#include <PIC/Vector.h>
+#include <PIC/UTL/Badge.h>
+#include <PIC/UTL/Range.h>
+#include <PIC/VT/FourTensor.h>
+#include <PIC/VT/Scalar.h>
+#include <PIC/VT/Tensor.h>
+#include <PIC/VT/Vector.h>
 
 #include <vector>
 
@@ -27,8 +28,8 @@ template <class Concrete>
 class RelativisticVDF {
     using Self = Concrete;
 
-    [[nodiscard]] constexpr auto self() const noexcept { return static_cast<Self const *>(this); }
-    [[nodiscard]] constexpr auto self() noexcept { return static_cast<Self *>(this); }
+    [[nodiscard]] constexpr decltype(auto) self() const noexcept { return static_cast<Self const *>(this); }
+    [[nodiscard]] constexpr decltype(auto) self() noexcept { return static_cast<Self *>(this); }
 
 protected:
     Geometry geomtr;
@@ -46,39 +47,39 @@ public:
 
     /// Plasma description associated with *this
     ///
-    [[nodiscard]] decltype(auto) plasma_desc() const noexcept { return self()->impl_plasma_desc(); }
+    [[nodiscard]] decltype(auto) plasma_desc() const noexcept { return self()->impl_plasma_desc({}); }
 
     /// Sample a single particle following the marker particle distribution, g0.
     /// \note Concrete subclass should provide impl_emit with the same signature.
     ///
-    [[nodiscard]] Particle emit() const { return self()->impl_emit(); }
+    [[nodiscard]] Particle emit() const { return self()->impl_emit({}); }
 
     /// Sample N particles following the marker particle distribution, g0.
     /// \note Concrete subclass should provide impl_emit with the same signature.
-    [[nodiscard]] std::vector<Particle> emit(unsigned long n) const { return self()->impl_emit(n); }
+    [[nodiscard]] std::vector<Particle> emit(unsigned long n) const { return self()->impl_emit({}, n); }
 
     /// Particle density scalar, \<1\>_0(x).
     /// \note Concrete subclass should provide impl_n with the same signature.
     ///
-    [[nodiscard]] Scalar n0(CurviCoord const &pos) const { return self()->impl_n(pos); }
+    [[nodiscard]] Scalar n0(CurviCoord const &pos) const { return self()->impl_n({}, pos); }
 
     /// Particle flux vector, \<v\>_0(x).
     /// \note Concrete subclass should provide impl_nV with the same signature.
     ///
-    [[nodiscard]] Vector nV0(CurviCoord const &pos) const { return self()->impl_nV(pos); }
+    [[nodiscard]] CartVector nV0(CurviCoord const &pos) const { return self()->impl_nV({}, pos); }
 
     /// Stress-energy four-tensor, \<ui*vj\>_0(x).
     /// \details Here u = {γc, γv} and v = {c, v}.
     /// \note Concrete subclass should provide impl_nuv with the same signature.
     ///
-    [[nodiscard]] FourTensor nuv0(CurviCoord const &pos) const { return self()->impl_nuv(pos); }
+    [[nodiscard]] FourCartTensor nuv0(CurviCoord const &pos) const { return self()->impl_nuv({}, pos); }
 
     /// Initial physical PSD
-    /// \details Concrete subclass should provide impl_f0 with the same signature.
-    [[nodiscard]] Real real_f0(Particle const &ptl) const { return self()->impl_f0(ptl); }
+    /// \details Concrete subclass should provide impl_f with the same signature.
+    [[nodiscard]] Real real_f0(Particle const &ptl) const { return self()->impl_f({}, ptl); }
 
     /// Ratio of the number of particles at the reference cell to the total number of particles
-    /// \note Concrete subclass should provide a member variable, m_Nrefcell_div_Ntotal, containing this quantity.
-    [[nodiscard]] Real Nrefcell_div_Ntotal() const { return self()->m_Nrefcell_div_Ntotal; }
+    /// \note Concrete subclass should provide a member variable, impl_Nrefcell_div_Ntotal, containing this quantity.
+    [[nodiscard]] Real Nrefcell_div_Ntotal() const { return self()->impl_Nrefcell_div_Ntotal({}); }
 };
 LIBPIC_NAMESPACE_END(1)
