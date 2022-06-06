c9df4ef31c20b933dbfe819295272e6f3588a446
Kyungguk Min <kyungguk@me.com>
2022-06-06 16:55:35 +0900

    LibPIC: Enable RelativisticVDF/MaxwellianVDF

--- src/LibPIC/PIC/RelativisticVDF/MaxwellianVDF.cc
+++ src/LibPIC/PIC/RelativisticVDF/MaxwellianVDF.cc
@@ -5,12 +5,12 @@
  */
 
 #include "MaxwellianVDF.h"
-#include "RandomReal.h"
+#include "../RandomReal.h"
+#include <algorithm>
 #include <cmath>
 
 LIBPIC_NAMESPACE_BEGIN(1)
-RelativisticMaxwellianVDF::RelativisticMaxwellianVDF(BiMaxPlasmaDesc const &desc, Geometry const &geo,
-                                                     Range const &domain_extent, Real c) noexcept
+RelativisticMaxwellianVDF::RelativisticMaxwellianVDF(BiMaxPlasmaDesc const &desc, Geometry const &geo, Range const &domain_extent, Real c)
 : RelativisticVDF{ geo, domain_extent, c }, desc{ desc }
 { // parameter check is assumed to be done already
     vth1_eq       = std::sqrt(desc.beta1) * c * std::abs(desc.Oc) / desc.op;
@@ -48,7 +48,7 @@ auto RelativisticMaxwellianVDF::q1(Real const N) const noexcept -> Real
     return std::atan(std::tan(sqrt_T2OT1_eq * geomtr.D1() * geomtr.xi() * N) / sqrt_T2OT1_eq) / (geomtr.xi() * geomtr.D1());
 }
 
-auto RelativisticMaxwellianVDF::impl_n(CurviCoord const &pos) const -> Scalar
+auto RelativisticMaxwellianVDF::impl_n(Badge<Super>, CurviCoord const &pos) const -> Scalar
 {
     constexpr Real n_eq = 1;
     return n_eq * eta(pos);
@@ -56,9 +56,9 @@ auto RelativisticMaxwellianVDF::impl_n(CurviCoord const &pos) const -> Scalar
 
 auto RelativisticMaxwellianVDF::n00c2(CurviCoord const &pos) const -> Scalar
 {
-    return impl_n(pos) * (c2 + .5 * vth1_eq * vth1_eq * (.5 + T2OT1(pos)));
+    return this->n0(pos) * (c2 + .5 * vth1_eq * vth1_eq * (.5 + T2OT1(pos)));
 }
-auto RelativisticMaxwellianVDF::P0Om0(CurviCoord const &pos) const -> Tensor
+auto RelativisticMaxwellianVDF::P0Om0(CurviCoord const &pos) const -> MFATensor
 {
     Real const vth1_squared = vth1_eq * vth1_eq;
     Real const T2OT1        = this->T2OT1(pos);
@@ -66,52 +66,57 @@ auto RelativisticMaxwellianVDF::P0Om0(CurviCoord const &pos) const -> Tensor
     Real const dP2          = std::sqrt(vth1_squared / c2 * (0.25 + T2OT1));
     Real const P1           = (1 - dP1) * (1 + dP1);
     Real const P2           = (1 - dP2) * (1 + dP2);
-    Real const factor       = *impl_n(pos) * vth1_squared / 2;
+    Real const factor       = *this->n0(pos) * vth1_squared / 2;
     return { factor * P1, factor * P2 * T2OT1, factor * P2 * T2OT1, 0, 0, 0 };
 }
-auto RelativisticMaxwellianVDF::impl_nuv(CurviCoord const &pos) const -> FourTensor
+auto RelativisticMaxwellianVDF::impl_nuv(Badge<Super>, CurviCoord const &pos) const -> FourCartTensor
 {
-    Scalar const n00c2 = this->n00c2(pos);
-    Tensor const P0Om0 = this->P0Om0(pos);
-    Vector const Vd    = { 0, 0, 0 };
-    Tensor const VV    = { Vd.x * Vd.x, 0, 0, 0, 0, 0 };
+    Scalar const    n00c2 = this->n00c2(pos);
+    MFATensor const P0Om0 = this->P0Om0(pos);
+    MFAVector const Vd    = { 0, 0, 0 };
+    MFATensor const VV    = { Vd.x * Vd.x, 0, 0, 0, 0, 0 };
 
     // in field-aligned lab frame
-    constexpr auto g2 = 1;
-    Scalar const   ED = g2 * (n00c2 + P0Om0.xx * Vd.x / c2);
-    Vector const   MD = g2 / c2 * (*n00c2 + P0Om0.xx) * Vd;
-    Tensor const   uv = P0Om0 + g2 / c2 * (*n00c2 + P0Om0.xx) * VV;
+    constexpr auto  g2 = 1;
+    Scalar const    ED = g2 * (n00c2 + P0Om0.xx * Vd.x / c2);
+    MFAVector const MD = g2 / c2 * (*n00c2 + P0Om0.xx) * Vd;
+    MFATensor const uv = P0Om0 + g2 / c2 * (*n00c2 + P0Om0.xx) * VV;
 
-    return { ED, geomtr.fac_to_cart(MD * c, pos), geomtr.fac_to_cart(uv, pos) };
+    return { ED, geomtr.mfa_to_cart(MD * c, pos), geomtr.mfa_to_cart(uv, pos) };
 }
 
-auto RelativisticMaxwellianVDF::f_common(Vector const &u, Real const T2OT1) noexcept -> Real
+auto RelativisticMaxwellianVDF::f_common(MFAVector const &u0, Real const T2OT1, Real const denom) noexcept -> Real
 {
-    // note that u0 = {γv1, γv2, γv3}/vth1 in co-moving frame
+    // note that the u0 is in co-moving frame and normalized by vth1
     // f0(u1, u2, u3) = exp(-u1^2)/√π * exp(-(u2^2 + u3^2)/(T2/T1))/(π T2/T1)
     //
-    Real const f1 = std::exp(-u.x * u.x) * M_2_SQRTPI * .5;
-    Real const x2 = u.y * u.y + u.z * u.z;
-    Real const f2 = std::exp(-x2 / T2OT1) / (M_PI * T2OT1);
-    return f1 * f2;
+    Real const f1   = std::exp(-u0.x * u0.x) * M_2_SQRTPI * .5;
+    Real const perp = u0.y * u0.y + u0.z * u0.z;
+    Real const f2   = std::exp(-perp / T2OT1) / (M_PI * T2OT1);
+    return (f1 * f2) / denom;
 }
-auto RelativisticMaxwellianVDF::f0(Vector const &vel, CurviCoord const &pos) const noexcept -> Real
+auto RelativisticMaxwellianVDF::f0(FourCartVector const &gcgvel, CurviCoord const &pos) const noexcept -> Real
 {
-    return Real{ impl_n(pos) } * f_common(geomtr.cart_to_fac(vel, pos) / vth1_eq, T2OT1(pos)) / vth1_eq_cubed;
+    // note that u = γ{v1, v2, v3} in lab frame, where γ = c/√(c^2 - v^2)
+    auto const gcgv_mfa = geomtr.cart_to_mfa(gcgvel, pos);
+    return Real{ this->n0(pos) } * f_common(gcgv_mfa.s / vth1_eq, T2OT1(pos), vth1_eq_cubed);
 }
-auto RelativisticMaxwellianVDF::g0(Vector const &vel, CurviCoord const &pos) const noexcept -> Real
+auto RelativisticMaxwellianVDF::g0(FourCartVector const &gcgvel, CurviCoord const &pos) const noexcept -> Real
 {
-    return Real{ impl_n(pos) } * f_common(geomtr.cart_to_fac(vel, pos) / marker_vth1_eq, T2OT1(pos)) / marker_vth1_eq_cubed;
+    // note that u = γ{v1, v2, v3} in lab frame, where γ = c/√(c^2 - v^2)
+    auto const gcgv_mfa = geomtr.cart_to_mfa(gcgvel, pos);
+    return Real{ this->n0(pos) } * f_common(gcgv_mfa.s / marker_vth1_eq, T2OT1(pos), marker_vth1_eq_cubed);
 }
 
-auto RelativisticMaxwellianVDF::impl_emit(unsigned long const n) const -> std::vector<Particle>
+auto RelativisticMaxwellianVDF::impl_emit(Badge<Super>, unsigned long const n) const -> std::vector<Particle>
 {
     std::vector<Particle> ptls(n);
-    for (auto &ptl : ptls)
-        ptl = emit();
+    std::generate(begin(ptls), end(ptls), [this] {
+        return this->emit();
+    });
     return ptls;
 }
-auto RelativisticMaxwellianVDF::impl_emit() const -> Particle
+auto RelativisticMaxwellianVDF::impl_emit(Badge<Super>) const -> Particle
 {
     Particle ptl = load();
 
@@ -139,16 +144,14 @@ auto RelativisticMaxwellianVDF::load() const -> Particle
     Real const phi1 = bit_reversed<3>() * 2 * M_PI;                               // [0, 2pi]
     Real const u1   = std::sqrt(-std::log(uniform_real<100>())) * std::sin(phi1); // γv_para
     //
-    Real const phi2   = bit_reversed<5>() * 2 * M_PI; // [0, 2pi]
-    Real const tmp_u2 = std::sqrt(-std::log(uniform_real<200>()) * T2OT1(pos));
-    Real const u2     = std::cos(phi2) * tmp_u2; // in-plane γv_perp
-    Real const u3     = std::sin(phi2) * tmp_u2; // out-of-plane γv_perp
+    Real const phi2 = bit_reversed<5>() * 2 * M_PI; // [0, 2pi]
+    Real const tmp  = std::sqrt(-std::log(uniform_real<200>()) * T2OT1(pos));
+    Real const u2   = std::cos(phi2) * tmp; // in-plane γv_perp
+    Real const u3   = std::sin(phi2) * tmp; // out-of-plane γv_perp
 
-    // velocity in Cartesian frame
-    //
-    Vector const g_vel = geomtr.fac_to_cart({ u1, u2, u3 }, pos) * marker_vth1_eq;
-    auto const   gamma = std::sqrt(1 + dot(g_vel, g_vel) / c2);
+    // boost from particle reference frame to co-moving frame
+    auto const gcgv_mfa = lorentz_boost<-1>(FourMFAVector{ c, {} }, MFAVector{ u1, u2, u3 } * (marker_vth1_eq / c));
 
-    return { g_vel, pos, gamma };
+    return { geomtr.mfa_to_cart(gcgv_mfa, pos), pos };
 }
 LIBPIC_NAMESPACE_END(1)
--- src/LibPIC/PIC/RelativisticVDF/MaxwellianVDF.h
+++ src/LibPIC/PIC/RelativisticVDF/MaxwellianVDF.h
@@ -18,7 +18,7 @@ LIBPIC_NAMESPACE_BEGIN(1)
 /// parallel to the background magnetic field direction, respectively.
 ///
 class RelativisticMaxwellianVDF : public RelativisticVDF<RelativisticMaxwellianVDF> {
-    friend RelativisticVDF<RelativisticMaxwellianVDF>;
+    using Super = RelativisticVDF<RelativisticMaxwellianVDF>;
 
     BiMaxPlasmaDesc desc;
     //
@@ -41,43 +41,49 @@ public:
     /// \param domain_extent Spatial domain extent.
     /// \param c Light speed. A positive real.
     ///
-    RelativisticMaxwellianVDF(BiMaxPlasmaDesc const &desc, Geometry const &geo, Range const &domain_extent, Real c) noexcept;
+    RelativisticMaxwellianVDF(BiMaxPlasmaDesc const &desc, Geometry const &geo, Range const &domain_extent, Real c);
 
-private:
-    [[nodiscard]] decltype(auto) impl_plasma_desc() const noexcept { return (this->desc); }
+    // VDF interfaces
+    //
+    [[nodiscard]] inline decltype(auto) impl_plasma_desc(Badge<Super>) const noexcept { return (this->desc); }
 
-    [[nodiscard]] inline Real eta(CurviCoord const &pos) const noexcept;
-    [[nodiscard]] inline Real T2OT1(CurviCoord const &pos) const noexcept;
-    [[nodiscard]] inline Real N(Real q1) const noexcept;
-    [[nodiscard]] inline Real q1(Real N) const noexcept;
+    [[nodiscard]] auto impl_n(Badge<Super>, CurviCoord const &pos) const -> Scalar;
+    [[nodiscard]] auto impl_nV(Badge<Super>, CurviCoord const &) const -> CartVector
+    {
+        return { 0, 0, 0 };
+    }
+    [[nodiscard]] auto impl_nuv(Badge<Super>, CurviCoord const &pos) const -> FourCartTensor;
 
-    // total energy density; weakly relativistic limit
-    [[nodiscard]] inline Scalar n00c2(CurviCoord const &pos) const;
-    // pressure tensor; weakly relativistic limit
-    [[nodiscard]] inline Tensor P0Om0(CurviCoord const &pos) const;
+    [[nodiscard]] Real impl_Nrefcell_div_Ntotal(Badge<Super>) const { return m_Nrefcell_div_Ntotal; }
+    [[nodiscard]] Real impl_f(Badge<Super>, Particle const &ptl) const { return f0(ptl); }
 
-    [[nodiscard]] Scalar     impl_n(CurviCoord const &pos) const;
-    [[nodiscard]] Vector     impl_nV(CurviCoord const &) const { return { 0, 0, 0 }; }
-    [[nodiscard]] FourTensor impl_nuv(CurviCoord const &pos) const;
+    [[nodiscard]] auto impl_emit(Badge<Super>, unsigned long) const -> std::vector<Particle>;
+    [[nodiscard]] auto impl_emit(Badge<Super>) const -> Particle;
 
-    [[nodiscard]] Real impl_f0(Particle const &ptl) const { return f0(ptl); }
-
-    [[nodiscard]] std::vector<Particle> impl_emit(unsigned long) const;
-    [[nodiscard]] Particle              impl_emit() const;
-    [[nodiscard]] Particle              load() const;
-
-    // velocity is normalized by vth1
-    [[nodiscard]] inline static Real f_common(Vector const &g_vel, Real T2OT1) noexcept;
-
-public:
     // equilibrium physical distribution function
     //
-    [[nodiscard]] Real f0(Vector const &g_vel, CurviCoord const &pos) const noexcept;
-    [[nodiscard]] Real f0(Particle const &ptl) const noexcept { return f0(ptl.g_vel, ptl.pos); }
+    [[nodiscard]] Real f0(FourCartVector const &gcgvel, CurviCoord const &pos) const noexcept;
+    [[nodiscard]] Real f0(Particle const &ptl) const noexcept { return f0(ptl.gcgvel, ptl.pos); }
 
     // marker particle distribution function
     //
-    [[nodiscard]] Real g0(Vector const &g_vel, CurviCoord const &pos) const noexcept;
-    [[nodiscard]] Real g0(Particle const &ptl) const noexcept { return g0(ptl.g_vel, ptl.pos); }
+    [[nodiscard]] Real g0(FourCartVector const &gcgvel, CurviCoord const &pos) const noexcept;
+    [[nodiscard]] Real g0(Particle const &ptl) const noexcept { return g0(ptl.gcgvel, ptl.pos); }
+
+private:
+    [[nodiscard]] Real eta(CurviCoord const &pos) const noexcept;
+    [[nodiscard]] Real T2OT1(CurviCoord const &pos) const noexcept;
+    [[nodiscard]] Real N(Real q1) const noexcept;
+    [[nodiscard]] Real q1(Real N) const noexcept;
+
+    // total energy density; weakly relativistic limit
+    [[nodiscard]] auto n00c2(CurviCoord const &pos) const -> Scalar;
+    // pressure tensor; weakly relativistic limit
+    [[nodiscard]] auto P0Om0(CurviCoord const &pos) const -> MFATensor;
+
+    [[nodiscard]] Particle load() const;
+
+    // velocity is normalized by vth1 and shifted to drifting plasma frame
+    [[nodiscard]] static auto f_common(MFAVector const &g_vel, Real T2OT1, Real denom) noexcept -> Real;
 };
 LIBPIC_NAMESPACE_END(1)
