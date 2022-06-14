6b1f141eeef627e9482bdabe0b6628bdbc4eadab
Kyungguk Min <kyungguk@me.com>
2022-06-14 21:17:34 +0900

    tests/pic_1d: Add test for whistler anisotropy instability

---
+++ tests/pic_1d/06_whistler_anisotropy_instability/run-pic_1d.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [[ $# -eq 0 ]] ; then
+    echo 'The first argument should be the number of MPI processes.'
+    exit 1
+fi
+
+n=$1
+target=pic_1d
+
+ninja $target || exit 1
+find ../data -type f -print -delete || exit 1
+/Library/com.kyungguk.usr.localized/mpich/3.2.1/bin/mpiexec -n $n ./src/$target/$target --wd ../data -record_particle_at_init -save --outer_Nt 400 --load=false || exit 1
+for i in {2..10}; do
+    /Library/com.kyungguk.usr.localized/mpich/3.2.1/bin/mpiexec -n $n ./src/$target/$target --wd ../data -record_particle_at_init -save --outer_Nt 400 --load=true || exit 1
+done
