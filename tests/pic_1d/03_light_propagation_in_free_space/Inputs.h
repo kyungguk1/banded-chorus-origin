5069e70a979ca5e4e2f7dea7a3dcf307f35c8436
Kyungguk Min <kyungguk@me.com>
2022-06-14 10:10:24 +0900

    tests/pic_1d: Add test for light propagation in free space

---
+++ tests/pic_1d/03_light_propagation_in_free_space/Inputs.h
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2022, Kyungguk Min
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+/// Simulation input parameters
+/// \details
+/// Modify the variables under the `Input' struct.
+///
+/// Consult/search headers under libPIC for unknown types/symbols.
+///
+struct Input {
+    //
+    // MARK:- Environment
+    //
+
+    /// Number of ghost cells
+    ///
+    /// It must be greater than 0.
+    ///
+    static constexpr unsigned number_of_ghost_cells = 3;
+
+    /// number of subdomains for domain decomposition (positive integer)
+    ///
+    /// Nx must be divisible by this number
+    ///
+    static constexpr unsigned number_of_subdomains = 1;
+
+    /// number of subdomain clones on which evenly divided particles are assigned and updated (positive integer)
+    ///
+    static constexpr unsigned number_of_distributed_particle_subdomain_clones = 1;
+
+    /// number of worker threads to spawn for parallelization
+    ///
+    /// value `0' means serial update; value `n' means parallelization using n + 1 threads
+    /// n + 1 must be divisible by number_of_subdomains * number_of_distributed_particle_subdomain_clones
+    ///
+    static constexpr unsigned number_of_worker_threads
+        = 1 * number_of_subdomains * number_of_distributed_particle_subdomain_clones - 1;
+
+    /// flag to suppress longitudinal and/or transverse components of the field fluctuations
+    ///
+    /// setting both to true will zero out all field fluctuations
+    ///
+    static constexpr bool should_neglect_transverse_component   = false;
+    static constexpr bool should_neglect_longitudinal_component = false;
+
+    /// particle boundary condition
+    ///
+    static constexpr BC particle_boundary_condition = BC::reflecting;
+
+    /// if set, randomize the gyro-phase of reflected particles
+    ///
+    static constexpr bool should_randomize_gyrophase_of_reflecting_particles = false;
+
+    /// wave masking function
+    ///
+    /// the first argument is masking inset, i.e., the number of grid points through which waves are gradually damped
+    /// the second argument is the masking coefficients, zero being no masking at all and one being 0 to 100% masking within the masking inset
+    ///
+    /// see `docs/boundary_condition.nb` for how the phase retardation and amplitude damping work
+    ///
+    static constexpr MaskingFunction phase_retardation{ 50, 1.0 };
+    static constexpr MaskingFunction amplitude_damping{ 50, 0.2 };
+
+    //
+    // MARK: Global parameters
+    //
+
+    /// light speed
+    ///
+    static constexpr Real c = 1;
+
+    /// magnitude of equatorial background magnetic field
+    ///
+    static constexpr Real O0 = 1;
+
+    /// inhomogeneity parameter, ξ
+    /// the field variation at the central field line is given by B/B_eq = 1 + (ξ*x)^2,
+    /// where x is the coordinate along the axis of mirror field symmetry
+    ///
+    static constexpr Real xi = 0;
+
+    /// simulation grid size at the equator
+    ///
+    static constexpr Real Dx = 0.5;
+
+    /// number of grid points
+    ///
+    static constexpr unsigned Nx = 960;
+
+    /// time step size
+    ///
+    static constexpr Real dt = 0.1;
+
+    /// number of time steps for inner loop
+    /// total time step Nt = inner_Nt * outer_Nt
+    /// simulation time t = dt*Nt
+    ///
+    static constexpr unsigned inner_Nt = 5;
+
+    /// number of time steps for outer loop
+    /// total time step Nt = inner_Nt * outer_Nt
+    /// simulation time t = dt*Nt
+    ///
+    static constexpr unsigned outer_Nt = 1000;
+
+    //
+    // MARK: Plasma Species Descriptions
+    //
+
+    /// kinetic plasma descriptors
+    ///
+    static constexpr auto part_descs = std::make_tuple();
+
+    /// cold fluid plasma descriptors
+    ///
+    static constexpr auto cold_descs = std::make_tuple();
+
+    /// external source descriptors
+    ///
+    static constexpr auto Next         = 1U;
+    static constexpr auto source_descs = std::make_tuple(
+        ExternalSourceDesc<Next>{
+            { 0.5, { 50, 2 * M_PI * 10 }, 20, 1 },
+            { ComplexVector{ 0, { 0, 0.04 }, { -0.04, 0 } } },
+            { CurviCoord{ 100 } },
+        });
+
+    //
+    // MARK: Data Recording
+    //
+
+    /// a top-level directory to which outputs will be saved
+    ///
+    static constexpr std::string_view working_directory = "./data";
+
+    /// field and particle energy density recording frequency; in units of inner_Nt
+    /// `0' means `not interested'
+    ///
+    static constexpr unsigned energy_recording_frequency = 1;
+
+    /// electric and magnetic field recording frequency
+    ///
+    static constexpr unsigned field_recording_frequency = 1;
+
+    /// species moment recording frequency
+    ///
+    static constexpr unsigned moment_recording_frequency = 0;
+
+    /// simulation particle recording frequency
+    ///
+    static constexpr unsigned particle_recording_frequency = 0;
+
+    /// maximum number of particles to dump
+    ///
+    static constexpr std::array<unsigned, std::tuple_size_v<decltype(part_descs)>> Ndumps
+        = {};
+
+    /// velocity histogram recording frequency
+    ///
+    static constexpr unsigned vhistogram_recording_frequency = 0;
+
+    /// per-species gyro-averaged velocity space specification used for sampling velocity histogram
+    ///
+    /// the parallel (v1) and perpendicular (v2) velocity specs are described by
+    /// the range of the velocity space extent and the number of velocity bins
+    ///
+    /// note that the Range type is initialized with an OFFSET (or location) and LENGTH
+    ///
+    /// recording histograms corresponding to specifications with the bin count being 0 will be
+    /// skipped over
+    ///
+    static constexpr std::array<std::pair<Range, unsigned>, std::tuple_size_v<decltype(part_descs)>>
+        v1hist_specs = {};
+    static constexpr std::array<std::pair<Range, unsigned>, std::tuple_size_v<decltype(part_descs)>>
+        v2hist_specs = {};
+};
+
+/// debugging options
+///
+namespace Debug {
+constexpr bool zero_out_electromagnetic_field = false;
+constexpr Real initial_efield_noise_amplitude = 0e0;
+constexpr bool should_use_unified_snapshot    = false;
+} // namespace Debug
