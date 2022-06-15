6524645c58ccad540711b0f47f33532309bef0ee
Kyungguk Min <kyungguk@me.com>
2022-06-15 18:32:13 +0900

    tests/pic_1d: Update the whistler propagation test in mirror field

--- tests/pic_1d/14_whistler_propagation/Inputs.h
+++ tests/pic_1d/14_whistler_propagation/Inputs.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Kyungguk Min
+ * Copyright (c) 2022, Kyungguk Min
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -14,6 +14,12 @@ struct Input {
     //
     // MARK:- Environment
     //
+
+    /// Number of ghost cells
+    ///
+    /// It must be greater than 0.
+    ///
+    static constexpr unsigned number_of_ghost_cells = 2;
 
     /// number of subdomains for domain decomposition (positive integer)
     ///
@@ -30,14 +36,15 @@ struct Input {
     /// value `0' means serial update; value `n' means parallelization using n + 1 threads
     /// n + 1 must be divisible by number_of_subdomains * number_of_distributed_particle_subdomain_clones
     ///
-    static constexpr unsigned number_of_worker_threads = 1 * number_of_subdomains * number_of_distributed_particle_subdomain_clones - 1;
+    static constexpr unsigned number_of_worker_threads
+        = 1 * number_of_subdomains * number_of_distributed_particle_subdomain_clones - 1;
 
     /// flag to suppress longitudinal and/or transverse components of the field fluctuations
     ///
     /// setting both to true will zero out all field fluctuations
     ///
     static constexpr bool should_neglect_transverse_component   = false;
-    static constexpr bool should_neglect_longitudinal_component = true;
+    static constexpr bool should_neglect_longitudinal_component = false;
 
     /// particle boundary condition
     ///
@@ -97,9 +104,7 @@ struct Input {
     /// total time step Nt = inner_Nt * outer_Nt
     /// simulation time t = dt*Nt
     ///
-    /// This is configurable through the command line option, `--outer_Nt=[0-9].*`
-    ///
-    static constexpr unsigned outer_Nt = 2000;
+    static constexpr unsigned outer_Nt = 1600;
 
     //
     // MARK: Plasma Species Descriptions
@@ -108,17 +113,24 @@ struct Input {
     /// kinetic plasma descriptors
     ///
     static constexpr auto part_descs = std::make_tuple(
-        // BiMaxPlasmaDesc({ { -1, 4, 1 }, 500, _2nd, delta_f }, 0.001)
-    );
+        BiMaxPlasmaDesc({ { -O0, c, 0 }, 500, _1st }, 0.0001)
+        );
 
     /// cold fluid plasma descriptors
     ///
-    static constexpr auto cold_descs = std::make_tuple(ColdPlasmaDesc({ -1, 4 }));
+    static constexpr auto cold_descs = std::make_tuple(
+        //ColdPlasmaDesc({ -O0, c })
+        );
 
     /// external source descriptors
     ///
+    static constexpr auto Next         = 1U;
     static constexpr auto source_descs = std::make_tuple(
-        ExternalSourceDesc<1>({ 0.5, { 50, 2 * M_PI * 20 }, 30, 1 }, { ComplexVector{ 0, { 0, 0.04 * c }, { -0.04 * c, 0 } } }, { CurviCoord{ 100 } }));
+        ExternalSourceDesc<Next>{
+            { 0.4, { 50, 2 * M_PI * 30 }, 30, 1 },
+            { ComplexVector{ 0, { 0, 0.04 * c }, { -0.04 * c, 0 } } },
+            { CurviCoord{ 100 } },
+        });
 
     //
     // MARK: Data Recording
@@ -126,9 +138,7 @@ struct Input {
 
     /// a top-level directory to which outputs will be saved
     ///
-    /// This setting is configurable through the command line option, `--wd <path_to_data_dump>`
-    ///
-    static constexpr char working_directory[] = "./data";
+    static constexpr std::string_view working_directory = "./data";
 
     /// field and particle energy density recording frequency; in units of inner_Nt
     /// `0' means `not interested'
@@ -161,7 +171,7 @@ struct Input {
     /// the parallel (v1) and perpendicular (v2) velocity specs are described by
     /// the range of the velocity space extent and the number of velocity bins
     ///
-    /// note that the Range type is initialized with the OFFSET (or location) and the LENGTH
+    /// note that the Range type is initialized with an OFFSET (or location) and LENGTH
     ///
     /// recording histograms corresponding to specifications with the bin count being 0 will be
     /// skipped over
@@ -177,4 +187,5 @@ struct Input {
 namespace Debug {
 constexpr bool zero_out_electromagnetic_field = false;
 constexpr Real initial_efield_noise_amplitude = 0e0;
+constexpr bool should_use_unified_snapshot    = false;
 } // namespace Debug
