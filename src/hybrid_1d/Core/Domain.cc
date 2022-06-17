408b15fb396c6405f9d8a72dbc55773173005f13
Kyungguk Min <kyungguk@me.com>
2022-06-17 21:02:17 +0900

    hybrid_1d: Update abstract Domain and Delegate

--- src/hybrid_1d/Core/Domain.cc
+++ src/hybrid_1d/Core/Domain.cc
@@ -1,10 +1,11 @@
 /*
- * Copyright (c) 2019-2021, Kyungguk Min
+ * Copyright (c) 2019-2022, Kyungguk Min
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #include "Domain.h"
+#include "../Core/ExternalSource.hh"
 
 HYBRID1D_BEGIN_NAMESPACE
 template <class... Ts, class Int, Int... Is>
