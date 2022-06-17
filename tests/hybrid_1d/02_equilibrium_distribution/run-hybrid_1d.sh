01c1ef976b0346c87ff24b43fd37e52dac0348de
Kyungguk Min <kyungguk@me.com>
2022-06-18 07:22:56 +0900

    tests/hybrid_1d: Add equilibrium distribution test in uniform field

---
+++ tests/hybrid_1d/02_equilibrium_distribution/run-hybrid_1d.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [[ $# -eq 0 ]] ; then
+    echo 'The first argument should be the number of MPI processes.'
+    exit 1
+fi
+
+n=$1
+target=hybrid_1d
+MPIEXEC=/Library/com.kyungguk.usr.localized/mpich/3.2.1/bin/mpiexec
+
+ninja $target || exit 1
+find ../data -type f -print -delete || exit 1
+$MPIEXEC -n $n ./src/$target/$target --wd ../data -record_particle_at_init -save --outer_Nt 20 --load=false || exit 1
+for i in {2..5}; do
+    $MPIEXEC -n $n ./src/$target/$target --wd ../data -record_particle_at_init -save --outer_Nt 20 --load=true || exit 1
+done
