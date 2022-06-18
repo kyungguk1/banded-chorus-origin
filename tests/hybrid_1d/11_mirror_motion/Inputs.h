dc1c2b32ebbf4b45f98b4711561b90de9edf9036
Kyungguk Min <kyungguk@me.com>
2022-06-18 13:46:37 +0900

    tests/hybrid_1d: Update the mirror motion test in mirror field

--- tests/hybrid_1d/11_mirror_motion/Inputs.h
+++ tests/hybrid_1d/11_mirror_motion/Inputs.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Kyungguk Min
+ * Copyright (c) 2022, Kyungguk Min
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -14,6 +14,12 @@ struct Input {
     //
     // MARK:- Environment
     //
+
+    /// Number of ghost cells
+    ///
+    /// It must be greater than 0.
+    ///
+    static constexpr unsigned number_of_ghost_cells = 3;
 
     /// number of subdomains for domain decomposition (positive integer)
     ///
@@ -30,7 +36,8 @@ struct Input {
     /// value `0' means serial update; value `n' means parallelization using n + 1 threads
     /// n + 1 must be divisible by number_of_subdomains * number_of_distributed_particle_subdomain_clones
     ///
-    static constexpr unsigned number_of_worker_threads = 1 * number_of_subdomains * number_of_distributed_particle_subdomain_clones - 1;
+    static constexpr unsigned number_of_worker_threads
+        = 1 * number_of_subdomains * number_of_distributed_particle_subdomain_clones - 1;
 
     /// electric field extrapolation method
     ///
@@ -98,8 +105,6 @@ struct Input {
     /// total time step Nt = inner_Nt * outer_Nt
     /// simulation time t = dt*Nt
     ///
-    /// This is configurable through the command line option, `--outer_Nt=[0-9].*`
-    ///
     static constexpr unsigned outer_Nt = 200;
 
     //
@@ -115,8 +120,8 @@ struct Input {
     static constexpr auto part_descs
         = std::make_tuple(TestParticleDesc<2>(
             { O0, 1 },
-            { Vector{ 3.141592653589794, 5.441398092702653, 0. }, { 5.441398092702654, 3.1415926535897927, 0. } },
-            { CurviCoord{ 0 }, /*                              */ CurviCoord{ 0 } }));
+            { MFAVector{ 3.141592653589794, 5.441398092702653, 0. }, { 5.441398092702654, 3.1415926535897927, 0. } },
+            { CurviCoord{ 0 }, /*                                   */ CurviCoord{ 0 } }));
 
     /// cold fluid plasma descriptors
     ///
@@ -132,9 +137,7 @@ struct Input {
 
     /// a top-level directory to which outputs will be saved
     ///
-    /// This setting is configurable through the command line option, `--wd <path_to_data_dump>`
-    ///
-    static constexpr char working_directory[] = "./data";
+    static constexpr std::string_view working_directory = "./data";
 
     /// field and particle energy density recording frequency; in units of inner_Nt
     /// `0' means `not interested'
@@ -145,7 +148,7 @@ struct Input {
     ///
     static constexpr unsigned field_recording_frequency = 0;
 
-    /// species moment recording frequency
+    /// ion species moment recording frequency
     ///
     static constexpr unsigned moment_recording_frequency = 0;
 
@@ -167,7 +170,7 @@ struct Input {
     /// the parallel (v1) and perpendicular (v2) velocity specs are described by
     /// the range of the velocity space extent and the number of velocity bins
     ///
-    /// note that the Range type is initialized with the OFFSET (or location) and the LENGTH
+    /// note that the Range type is initialized with an OFFSET (or location) and LENGTH
     ///
     /// recording histograms corresponding to specifications with the bin count being 0 will be
     /// skipped over
@@ -183,4 +186,5 @@ struct Input {
 namespace Debug {
 constexpr bool zero_out_electromagnetic_field = true;
 constexpr Real initial_bfield_noise_amplitude = 0e0;
+constexpr bool should_use_unified_snapshot    = false;
 } // namespace Debug
